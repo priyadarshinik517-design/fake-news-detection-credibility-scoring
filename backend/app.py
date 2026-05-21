7504813134ed8720a878ec25f2dde8344bc28d3b
Priyadarshini K <priyadarshinik517@gmail.com>
2026-05-21 12:17:00 +0530

    Fixed Render port issue

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -34,4 +35,5 @@ def predict():
 
 # Run app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
