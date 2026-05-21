0aee7c4926a1cbe6ad53f158d64304cb2183a1ca
Priyadarshini K <priyadarshinik517@gmail.com>
2026-05-21 12:09:21 +0530

    Fixed Render hosting

--- backend/app.py
+++ backend/app.py
@@ -5,20 +5,25 @@ import pickle
 app = Flask(__name__)
 CORS(app)
 
+# Load model and vectorizer
 model = pickle.load(open('model.pkl', 'rb'))
 vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
 
+# Home route
 @app.route('/')
 def home():
     return "Fake News Detection Backend Running"
 
+# Prediction route
 @app.route('/predict', methods=['POST'])
 def predict():
 
     data = request.get_json()
+
     text = data['text']
 
     vector = vectorizer.transform([text])
+
     prediction = model.predict(vector)[0]
 
     result = "REAL" if prediction == 1 else "FAKE"
@@ -27,5 +32,6 @@ def predict():
         'prediction': result
     })
 
+# Run app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
